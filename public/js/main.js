f884af510312052f4ac30a3abfaee0d9f8f7220f
raphael <raphael.a@rgsistemas.com.br>
2017-01-30 13:36:10 -0200

    Aula 2 Parte 1 - Incluído função val

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,16 @@
+/*
+ * Scrapbook
+ *
+ * .val() 			-> Funciona em <input>(todos os tipos), <textarea> e <select>
+ * .text() 			-> Funciona em <h1>, <span> e <p> 
+ * .val() e .text() -> Ambas funções podem atribuir novos valores ou pegar os valores de texto dos elementos
+ *
+ */
+
+
+
+
+
 // Obtem o texto dentro da class frase
 var frase = $(".frase").text();
 
@@ -8,4 +21,17 @@ var numPalavras = frase.split(" ").length;
 var tamanhoFrase = $("#tamanho-frase");
 tamanhoFrase.text(numPalavras);
 
-console.log(tamanhoFrase);
+
+// on é uma função de evento do jQuery
+// Primeiro parâmetro é o tipo do evento e o segundo é o que você quer que realize
+var campo = $(".campo-digitacao");
+campo.on("input", function() {
+
+	// /\S+/ é uma expressão regular que busca qualquer caractere, exceto espaço vazio
+	var conteudo = campo.val();
+	var qtdPalavras = conteudo.split(/\S+/).length -1;
+	$("#contador-palavras").text(qtdPalavras);
+
+	var qtdCaracteres = conteudo.length;
+	$("#contador-caracteres").text(qtdCaracteres);
+});

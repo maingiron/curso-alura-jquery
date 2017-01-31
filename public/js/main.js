d6c2b6825d81b02cab9ae215a08554f37b629d0e
Raphael Giron <maingiron@gmail.com>
2017-01-31 21:02:00 -0200

    Aula 4 Parte 1 - Organização do JS em functions e incluído botão de reiniciar jogo

--- public/js/main.js
+++ public/js/main.js
@@ -27,50 +27,87 @@
 
 
 
-
-
-// Obtem o texto dentro da class frase
-var frase = $(".frase").text();
-
-// Retorna o tamanho do array com as palavras separadas
-var numPalavras = frase.split(" ").length;
-
-// Seta a variável no id e atribui um novo valor de acordo com o números de palavras
-var tamanhoFrase = $("#tamanho-frase");
-tamanhoFrase.text(numPalavras);
-
-
-// Primeiro parâmetro é o tipo do evento e o segundo é o que você quer que realize
 var campo = $(".campo-digitacao");
-campo.on("input", function() {
-
-	// /\S+/ é uma expressão regular que busca qualquer caractere, exceto espaço vazio
-	var conteudo = campo.val();
-	var qtdPalavras = conteudo.split(/\S+/).length -1;
-	$("#contador-palavras").text(qtdPalavras);
-
-	var qtdCaracteres = conteudo.length;
-	$("#contador-caracteres").text(qtdCaracteres);
-});
+var tempoInicial = $("#tempo-digitacao").text();
 
 
-var tempoRestante = $("#tempo-digitacao").text();
-campo.one("focus", function() {
 
-	// setInterval é uma função do JavaScript que faz com que uma determinada ação (passada como primeiro parâmetro) seja executada em um intervalo de tempo (passado como segundo parâmetro)
-	var cronometroID = setInterval(function() {
+// A função $() é um atalho da $(document).ready(function()
+$(function (){
 
-		tempoRestante--;
-		$("#tempo-digitacao").text(tempoRestante);
-
-		if(tempoRestante < 1) {
-			// .attr é uma função do jQuery que pode pegar, incluir ou modificar um valor de atributo
-			// disabled não tem nenhum valor, por isso, precisar passar o valor true para a função
-			campo.attr("disabled", true);
-			clearInterval(cronometroID);
-		}
-		
-
-	}, 1000);
+	atualizaTamanhoFrase();
+	inicializaContadores();
+	inicializaCronometro();
+	$("#botao-reiniciar").click(reiniciaJogo);
 
 });
+
+
+
+
+function atualizaTamanhoFrase() {
+
+	var frase = $(".frase").text();
+	var numPalavras = frase.split(" ").length;
+	var tamanhoFrase = $("#tamanho-frase");
+	tamanhoFrase.text(numPalavras);
+
+}
+
+
+function inicializaContadores() {
+
+	campo.on("input", function() {
+
+		// /\S+/ é uma expressão regular que busca qualquer caractere, exceto espaço vazio
+		var conteudo = campo.val();
+		var qtdPalavras = conteudo.split(/\S+/).length -1;
+		$("#contador-palavras").text(qtdPalavras);
+
+		var qtdCaracteres = conteudo.length;
+		$("#contador-caracteres").text(qtdCaracteres);
+	});
+
+}
+
+
+function inicializaCronometro() {
+
+	var tempoRestante = $("#tempo-digitacao").text();
+	campo.one("focus", function() {
+
+		$("#botao-reiniciar").attr("disabled", true);
+
+		// setInterval é uma função do JavaScript que faz com que uma determinada ação (passada como primeiro parâmetro) seja executada em um intervalo de tempo (passado como segundo parâmetro)
+		var cronometroID = setInterval(function() {
+
+			tempoRestante--;
+			$("#tempo-digitacao").text(tempoRestante);
+
+			if(tempoRestante < 1) {
+				// .attr é uma função do jQuery que pode pegar, incluir ou modificar um valor de atributo
+				// disabled não tem nenhum valor, por isso, precisar passar o valor true para a função
+				campo.attr("disabled", true);
+				clearInterval(cronometroID);
+				$("#botao-reiniciar").attr("disabled", false);
+			}
+			
+
+		}, 1000);
+
+	});
+
+}
+
+
+function reiniciaJogo() {
+
+	campo.val("");
+	campo.attr("disabled", false);
+	$("#contador-palavras").text(0);
+	$("#contador-caracteres").text(0);
+	$("#tempo-digitacao").text(tempoInicial);
+
+	inicializaCronometro();
+
+}

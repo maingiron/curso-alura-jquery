f810c91cad6424d8a536292d7610c7a8bb2411a4
Raphael Giron <maingiron@gmail.com>
2017-02-06 21:11:53 -0200

    Aula 1 Parte 2 - Incluído funções de animações

--- public/js/placar.js
+++ public/js/placar.js
@@ -1,9 +1,46 @@
+/*
+ * Scrapbook
+ *
+ *
+ * .show()				-> Adiciona um elemento (display block)
+ * .hide()				-> Esconde um elemento (display none)
+ * .toggle()			-> Adiciona ou esconde um elemento (display block/none)
+ * .slideDown()			-> Mesma funcionalidade que a função show(), mas realiza a transição de uma maneira mais suave
+ * .slideUp()			-> Mesma funcionalidade que a função hide(), mas realiza a transição de uma maneira mais suave
+ * .slideToggle() 		-> Realiza a função do slideDown() e slideUp()
+ *	
+ * .slideDown(100)
+ * .slideUp(200)
+ * .slideToggle(300)
+ * .fadeOut(400)
+ * .fadeIn(500)
+ * .fadeToggle(600) 	-> Argumento em milissegundos para realizar a transição
+ *
+ * .fadeOut()			-> Remove a opacidade do elemento até um ponto e depois modifica a sua propriedade display
+ * .fadeIn()			-> Adiciona a opacidade do elemento até um ponto e depois modifica a sua propriedade display
+ * .fadeToggle()		-> Realiza a função do fadeOut() e fadeIn()
+ *
+ * .stop() 				-> Interrompe uma animação que está acontecendo e a próxima é iniciada
+ *
+ * .offset() 			-> Retorna a distância em que o elemento está do topo e da esquerda da página
+ *
+ * .animate() 			-> Realiza a animação e recebe dois parâmetros, um objeto que contém as propriedades CSS a serem animadas e os seus valores, e o tempo de duração da animação
+ *
+ *
+ *
+ */
+
+
+
+$("#botao-placar").click(mostraPlacar);
+
+
 function inserePlacar() {
 
 	// .find realiza uma busca no filho do elemento
 	// .find recebo como parâmetro seletores CSS
 	var corpoTabela = $(".placar").find("tbody");
-	var usuario = "Raphael";
+	var usuario = "Usuário Padrão";
 	var numPalavras = $("#contador-palavras").text();
 
 	var linha = novaLinha(usuario, numPalavras);
@@ -13,6 +50,23 @@ function inserePlacar() {
 	// prepend adiciona a string/HTML no primeiro filho do elemento selecionado
 	corpoTabela.prepend(linha);
 
+	$(".placar").slideDown(500);
+	scrollPlacar();
+
+}
+
+
+function scrollPlacar() {
+
+	// offset retorna a distância em que o elemento está do topo e da esquerda da página
+	var posicaoPlacar = $(".placar").offset().top;
+
+	// animate realiza a animação e recebe dois parâmetros, um objeto que contém as propriedades CSS a serem animadas e os seus valores, e o tempo de duração da animação
+	$("body").animate( {
+
+		scrollTop: posicaoPlacar + "px"
+	}, 1000);
+	
 }
 
 
@@ -46,6 +100,21 @@ function removeLinha() {
 	event.preventDefault();
 
 	// .parent pega o pai do this
-	$(this).parent().parent().remove();
+	var linha = $(this).parent().parent(); 
+
+	linha.fadeOut(1000);
+
+	setTimeout(function() {
+		
+		linha.remove();
+	}, 1000);
+	
+}
+
+
+function mostraPlacar() {
+
+	// .stop interrompe uma animação que está acontecendo e a próxima é iniciada
+	$(".placar").stop().slideToggle(600);
 
 }
